1147d0bf1199cb92a55c520249976d8a5c1c0ca3
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-17 23:36:49 +0800

    task6: use service

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,16 +1,18 @@
 import { Controller, Get, Headers, Param, Post, Body } from '@nestjs/common';
 import { CreatePostDto } from './post.dto';
+import { DemoService } from './providers/demo/demo.service';
 
 @Controller('posts')
 export class PostsController {
+    private readonly demoService;
+
+    constructor(demoService: DemoService){
+        this.demoService = demoService;
+    }
+
     @Get()
-    index(@Headers('authorization') headers) {
-        console.log(headers);
-        return [
-            { 
-                title:'posts'
-            }
-        ];
+    index() {
+        return this.demoService.findAll();
     }
 
     @Get(':id')
@@ -22,6 +24,6 @@ export class PostsController {
 
     @Post()
     store(@Body() post: CreatePostDto) {
-        console.log(post.title);
+        this.demoService.create(post);
     }
 }
