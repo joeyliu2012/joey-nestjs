dc63d03242f81caea4078db3dd779f1f58d18538
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-19 21:08:37 +0800

    拦截器：转换异常

--- src/modules/posts/posts.controller.ts
+++ src/modules/posts/posts.controller.ts
@@ -6,6 +6,7 @@ import { DemoAuthGuard } from '../../core/guards/demo-auth.guard';
 import { Roles } from '../../core/decorators/roles.decorator';
 import { LogginInterceptor } from '../../core/interceptors/loggin.interceptor';
 import { TransformInterceptor } from '../../core/intereptors/transform.interceptor';
+import { ErrorsInterceptor } from '../../core/interceptors/errors.interceptor';
 
 @Controller('posts')
 // @UseFilters(DemoFilter)
@@ -15,9 +16,11 @@ export class PostsController {
     constructor(private readonly demoService: DemoService){}
 
     @Get()
-    @UseInterceptors(TransformInterceptor)
+    // @UseInterceptors(TransformInterceptor)
+    @UseInterceptors(ErrorsInterceptor)
     index() {
-        return this.demoService.findAll();
+        throw new ForbiddenException();
+        // return this.demoService.findAll();
     }
 
     @Get(':id')
