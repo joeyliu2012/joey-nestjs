ea837e5fd10b35d728c4d6ee1ced50423dced6da
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-19 17:48:17 +0800

    拦截器（Interceptors）

---
+++ src/core/interceptors/loggin.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { LogginInterceptor } from './loggin.interceptor';
+
+describe('LogginInterceptor', () => {
+  it('should be defined', () => {
+    expect(new LogginInterceptor()).toBeDefined();
+  });
+});
