5d2f7dcb2de649c16960a532d66762f21b661c4c
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-20 11:02:08 +0800

    update

--- src/core/interceptors/loggin.interceptor.spec.ts
+++
@@ -1,7 +0,0 @@
-import { LogginInterceptor } from './loggin.interceptor';
-
-describe('LogginInterceptor', () => {
-  it('should be defined', () => {
-    expect(new LogginInterceptor()).toBeDefined();
-  });
-});
