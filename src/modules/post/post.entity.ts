aa86c9d0b07f9cadbf5e01d2cbdd1c2d931aa76a
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-20 19:06:05 +0800

    实体里的数据栏（Column）

--- src/modules/post/post.entity.ts
+++ src/modules/post/post.entity.ts
@@ -1,8 +1,20 @@
-import { Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from "typeorm";
 
 @Entity()
 
 export class Post {
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column()
+    title: string;
+
+    @Column('longtext')
+    body: string;
+
+    @CreateDateColumn()
+    created: Date;
+
+    @CreateDateColumn()
+    updated: Date;
 }
