39246c68b16889cc038c1ab9d3eb8f36e23077ef
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-22 00:15:39 +0800

    更新单个实体数据

--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Put } from '@nestjs/common';
 import { PostService } from './post.service';
 import { async } from 'rxjs/internal/scheduler/async';
 
@@ -21,6 +21,11 @@ export class PostController {
     @Get(':id')
     async show(@Param('id') id: string) {
         return await this.postService.show(id);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id:string, @Body() data) {
+        return await this.postService.update(id, data);
     }
 
 }
