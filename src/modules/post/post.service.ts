78365cfbb50cbcf429a2cd77a4eaf3ab7d1be5bb
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-21 22:29:46 +0800

    注册实体的 Repository

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Post } from './post.entity';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+    constructor(
+        @InjectRepository(Post)
+        private readonly postRepository: Repository<Post>
+    ) {}
+}
