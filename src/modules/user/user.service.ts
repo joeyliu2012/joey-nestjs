ca4b7564eb7e5cb9f470a59d7b2b4ea6bf138838
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-24 00:36:29 +0800

    定义用户登录接口（auth/login）

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -50,4 +50,8 @@ export class UserService {
 
         return await this.userRepository.save(entity);
     }
+
+    async findByName(name: string) {
+        return await this.userRepository.findOne({ name });
+    }
 }
