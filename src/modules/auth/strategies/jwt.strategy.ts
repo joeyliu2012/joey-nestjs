21ea703c67c13aff33796ed683700ed230528e0d
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-25 18:11:56 +0800

    验证身份的方法（validate）

--- src/modules/auth/strategies/jwt.strategy.ts
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -1,18 +1,27 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { Strategy, ExtractJwt, VerifiedCallback } from "passport-jwt";
 import { JwtPayload } from "../auth.interface";
+import { UserService } from "../../user/user.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-    constructor() {
+    constructor(private readonly userService: UserService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: 'qHqPHVPasjfHDCrcX7Ao7Ao7x505W098RU3i6lloVgWZFY='
         })
     }
     
-    async validate(payload: JwtPayload, done: VerifiedCallback) {
+    async validate(payload: JwtPayload) {
         console.log('payload:', payload);
+        const { name } = payload;
+        const entity = await this.userService.findByName(name);
+
+        if(!entity) {
+            throw new UnauthorizedException('没找到用户。');
+        }
+
+        return entity;
     }
 }
