e7d630d24fda1027aeeb593425db7952c71aabf7
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-27 17:45:07 +0800

    存储内容评论 with bug

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UseGuards, UseInterceptors, ClassSerializerInterceptor, Param, ParseIntPipe, Body } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { CommentDto } from './comment.dto';
+import { CommentService } from './comment.service';
+import { User as UserEntity } from '../user/user.entity';
+import { User } from '../../core/decorators/user.decorator';
 
-@Controller('comment')
-export class CommentController {}
+@Controller()
+export class CommentController {
+    constructor(
+        private readonly commentService: CommentService
+    ) {}
+
+    @Post('posts/:id/comments')
+    @UseGuards(AuthGuard())
+    @UseInterceptors(ClassSerializerInterceptor)
+    async storePostComment(
+        @Param(' id', ParseIntPipe) id: number,
+        @Body() data: CommentDto,
+        @User() user: UserEntity
+    ) {
+        return await this.commentService.storePostComment(id, user, data);
+    }
+}
---
+++ src/modules/comment/comment.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../user/user.entity';
+import { CommentDto } from './comment.dto';
+import { Comment } from './comment.entity';
+
+@Injectable()
+export class CommentService {
+    constructor(
+        @InjectRepository(Comment)
+        private readonly commentRepository: Repository<Comment>
+    ) {}
+
+    async storePostComment(id: number, user: User, data: CommentDto) {
+        return await this.commentRepository.save({
+            user,
+            ...data,
+            post: { id }
+        });
+    }
+}
