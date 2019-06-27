de9ee564e8c4b8653cfef2e6ebd1a20e1b1f8a77
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-27 08:11:12 +0800

    处理标签实体数据

--- src/modules/tag/tag.controller.ts
+++ src/modules/tag/tag.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Put, Param, ParseIntPipe, Delete } from '@nestjs/common';
+import { TagService } from './tag.service';
+import { async } from 'rxjs/internal/scheduler/async';
+import { TagDto } from '../../../dist/modules/tag/tag.dto';
 
-@Controller('tag')
-export class TagController {}
+@Controller('tags')
+export class TagController {
+    constructor(
+        private readonly tagService: TagService
+    ) {}
+
+    @Post()
+    async store(@Body() data: TagDto) {
+        return await this.tagService.store(data);
+    }
+
+    @Put(':id')
+    async update(@Param('id', ParseIntPipe) id: number, @Body() data: TagDto) {
+        return await this.tagService.update(id, data);
+    }
+
+    @Delete(':id')
+    async delete(@Param('id', ParseIntPipe) id: number) {
+        return await this.tagService.destory(id);
+    }
+}
--- src/modules/tag/tag.service.ts
+++ src/modules/tag/tag.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Tag } from './tag.entity';
+import { TagDto } from './tag.dto';
 
 @Injectable()
-export class TagService {}
+export class TagService {
+    constructor(
+        @InjectRepository(Tag)
+        private readonly tagRepository: Repository<Tag>
+    ) {}
+
+    async store(data: TagDto) {
+        return await this.tagRepository.save(data);
+    }
+
+    async update(id: number, data: TagDto) {
+        return await this.tagRepository.update(id, data);
+    }
+
+    async destory(id: number) {
+        return await this.tagRepository.delete(id);
+    }
+}
