dd36c4727ed3f6b2317ef3fdc9364087fde7a751
joeyliu2012 <joeyliu2012@gmail.com>
2019-06-29 19:06:34 +0800

    在用户服务添加检查用户是否拥有指定资源的方法（possess）

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -9,13 +9,13 @@ export class UserService {
     constructor(
         @InjectRepository(User)
         private readonly userRepository: Repository<User>
-    ) {}
+    ) { }
 
     async store(data: UserDto) {
         const { name } = data;
         const user = await this.userRepository.findOne({ name });
 
-        if(user) {
+        if (user) {
             throw new BadRequestException('用户已经存在了。');
         }
 
@@ -28,10 +28,10 @@ export class UserService {
         const entity = await this.userRepository.findOne(id, {
             relations: ['posts']
         });
-        
-        if(!entity) {
+
+        if (!entity) {
             throw new NotFoundException('没找到用户。');
-        }   
+        }
         return entity;
     }
 
@@ -39,13 +39,13 @@ export class UserService {
         const { password, newPassword } = data;
         const entity = await this.userRepository.findOne(id);
 
-        if(!entity) {
+        if (!entity) {
             throw new NotFoundException('没有找到用户。');
         }
 
         const pass = await entity.comparePassword(password);
 
-        if(!pass) {
+        if (!pass) {
             throw new BadRequestException('密码验证失败，请重新输入正确的密码。');
         }
         entity.password = newPassword;
@@ -56,34 +56,49 @@ export class UserService {
     async findByName(name: string, password?: boolean) {
         const queryBuilder = await this.userRepository
             .createQueryBuilder('user');
-        
+
         queryBuilder
             .where('user.name = :name', { name })
             .leftJoinAndSelect('user.roles', 'roles');
 
-        if(password) {
+        if (password) {
             queryBuilder.addSelect('user.password');
         }
 
         const entity = queryBuilder.getOne();
-        
+
         return entity;
     }
 
     async liked(id: number) {
         return this.userRepository
-            .findOne(id, {relations: ['voted', 'voted.user']});
+            .findOne(id, { relations: ['voted', 'voted.user'] });
     }
 
     async update(id: number, data: UserDto) {
-        const {roles} = data;
+        const { roles } = data;
 
         const entity = await this.userRepository.findOne(id);
 
-        if(roles) {
+        if (roles) {
             entity.roles = roles;
         }
 
         return await this.userRepository.save(entity);
+    }
+
+    async possess(
+        id: number,
+        resource: string,
+        resourceId: number
+    ) {
+        const result = await this.userRepository
+            .createQueryBuilder('user')
+            .where('user.id = :id', { id })
+            .leftJoin(`user.${resource}`, resource)
+            .andWhere(`${resource}.id = :resourceId`, { resourceId })
+            .getCount();
+
+        return result === 1 ? true : false;
     }
 }
